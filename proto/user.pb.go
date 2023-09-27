60287247a44adc248f8b6f7e61f758bd65074259
lukmannudin <lukmannudin@luno.com>
2023-09-27 23:23:09 +0700

    teuing

--- proto/user.pb.go
+++ proto/user.pb.go
@@ -138,6 +138,53 @@ func (x *UserResponse) GetEmail() string {
 	return ""
 }
 
+type UserResponseLogin struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Token string `protobuf:"bytes,1,opt,name=token,proto3" json:"token,omitempty"`
+}
+
+func (x *UserResponseLogin) Reset() {
+	*x = UserResponseLogin{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_user_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *UserResponseLogin) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UserResponseLogin) ProtoMessage() {}
+
+func (x *UserResponseLogin) ProtoReflect() protoreflect.Message {
+	mi := &file_user_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UserResponseLogin.ProtoReflect.Descriptor instead.
+func (*UserResponseLogin) Descriptor() ([]byte, []int) {
+	return file_user_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *UserResponseLogin) GetToken() string {
+	if x != nil {
+		return x.Token
+	}
+	return ""
+}
+
 var File_user_proto protoreflect.FileDescriptor
 
 var file_user_proto_rawDesc = []byte{
@@ -151,9 +198,12 @@ var file_user_proto_rawDesc = []byte{
 	0x55, 0x73, 0x65, 0x72, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x12, 0x0a, 0x04,
 	0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65,
 	0x12, 0x14, 0x0a, 0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52,
-	0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x42, 0x16, 0x5a, 0x14, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62,
-	0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6f, 0x6c, 0x65, 0x67, 0x6d, 0x6f, 0x6e, 0x65, 0x79, 0x62, 0x06,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x22, 0x29, 0x0a, 0x11, 0x55, 0x73, 0x65, 0x72, 0x52, 0x65,
+	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x4c, 0x6f, 0x67, 0x69, 0x6e, 0x12, 0x14, 0x0a, 0x05, 0x74,
+	0x6f, 0x6b, 0x65, 0x6e, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x74, 0x6f, 0x6b, 0x65,
+	0x6e, 0x42, 0x16, 0x5a, 0x14, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f,
+	0x6f, 0x6c, 0x65, 0x67, 0x6d, 0x6f, 0x6e, 0x65, 0x79, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x33,
 }
 
 var (
@@ -168,10 +218,11 @@ func file_user_proto_rawDescGZIP() []byte {
 	return file_user_proto_rawDescData
 }
 
-var file_user_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_user_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_user_proto_goTypes = []interface{}{
-	(*UserRequest)(nil),  // 0: olegmoney.UserRequest
-	(*UserResponse)(nil), // 1: olegmoney.UserResponse
+	(*UserRequest)(nil),       // 0: olegmoney.UserRequest
+	(*UserResponse)(nil),      // 1: olegmoney.UserResponse
+	(*UserResponseLogin)(nil), // 2: olegmoney.UserResponseLogin
 }
 var file_user_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -211,6 +262,18 @@ func file_user_proto_init() {
 				return nil
 			}
 		}
+		file_user_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*UserResponseLogin); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -218,7 +281,7 @@ func file_user_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_user_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
--- server/user.go
+++ server/user.go
@@ -3,13 +3,13 @@ package main
 import (
 	"context"
 	"errors"
-	"log"
-	"os"
-
+	"fmt"
 	"github.com/golang-jwt/jwt"
 	pb "github.com/olegmoney/proto"
 	"github.com/olegmoney/server/config"
 	"golang.org/x/crypto/bcrypt"
+	"log"
+	"os"
 )
 
 type Users struct {
@@ -27,24 +27,30 @@ func (s *Server) SignUp(ctx context.Context, in *pb.UserRequest) (*pb.UserRespon
 	}, nil
 }
 
-func (s *Server) SignIn(ctx context.Context, payload *pb.UserRequest) (*pb.UserResponse, error) {
+func (s *Server) SignIn(ctx context.Context, payload *pb.UserRequest) (*pb.UserResponseLogin, error) {
 	email := payload.Email
 
+	fmt.Println("HIHIHAHA")
+	fmt.Println(payload)
 	// Get Supabase connection
 	supabase := config.GetConnectionSupabse()
 
-	var result []struct {
-		Id       int    `json:"id"`
-		Email    string `json:"email"`
-		Name     string `json:"name"`
-		Password string `json:"password"`
-	}
+	//var result []struct {
+	//	Id       int    `json:"id"`
+	//	Email    string `json:"email"`
+	//	Name     string `json:"name"`
+	//	Password string `json:"password"`
+	//}
+
+	var result map[string]interface{}
 
 	// Query the database for the user by email
-	errorDb := supabase.DB.From("users").Select("*").Eq("email", email).Execute(&result)
+	//errorDb := supabase.DB.From("users").Select("*").Eq("email", email).Execute(&result)
+	errorDb := supabase.DB.From("users").Select("*").Limit(1).Eq("email", email).Execute(&result)
 
 	if errorDb != nil {
 		// Handle the error gracefully instead of panicking
+		fmt.Println(errorDb)
 		return nil, errorDb
 	}
 
@@ -52,13 +58,15 @@ func (s *Server) SignIn(ctx context.Context, payload *pb.UserRequest) (*pb.UserR
 		// Handle the case where no user was found with the given email
 		return nil, errors.New("user not found")
 	}
+	fmt.Println(result)
 
-	password := result[0].Password
+	password := result["password"].(string)
 
 	// Compare the stored password hash with the provided password
 	err := bcrypt.CompareHashAndPassword([]byte(password), []byte(payload.Password))
 	if err != nil {
 		// Handle incorrect password
+		fmt.Println(err)
 		return nil, errors.New("incorrect password")
 	}
 
@@ -66,8 +74,8 @@ func (s *Server) SignIn(ctx context.Context, payload *pb.UserRequest) (*pb.UserR
 	token, err := sign.SignedString([]byte(os.Getenv("JWT_SECRET")))
 	if err != nil {
 		return nil, errors.New("error when generate token")
-
 	}
+	fmt.Println(token)
 	// Password is correct, return a success response
 	return &pb.UserResponseLogin{
 		Token: token,
